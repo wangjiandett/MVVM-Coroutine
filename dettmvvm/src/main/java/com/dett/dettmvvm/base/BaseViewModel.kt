09cc0bad209002e3f26e32c96946b8cc6eb78e8f
DESKTOP-3QE1CF3\JWZ <18255812369@qq.com>
2020-12-19 15:24:29 +0800

    完善注释

--- dettmvvm/src/main/java/com/dett/dettmvvm/base/BaseViewModel.kt
+++ dettmvvm/src/main/java/com/dett/dettmvvm/base/BaseViewModel.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 /**
- * Describe
+ * BaseViewModel
  *
  * @author wangjian
  * Created on 2020/10/24 15:50
@@ -24,8 +24,23 @@ open class BaseViewModel<T> : ViewModel() {
 
     /**
      * 所有协程网络请求都在 viewModelScope 域中启动，当页面销毁时会自动
-     * 调用ViewModel的  #onCleared 方法取消所有协程
+     * 调用ViewModel的  #onCleared 方法取消所有协程，即调用 Job.cancel()方法
      */
     fun launchUI(block: suspend CoroutineScope.() -> Unit) = viewModelScope.launch { block() }
 
+
+    /**
+     * 自定义监听器，在界面销毁时取消请求
+     * ViewModel中已经自动实现了 cancel，无需手动实现
+     */
+//    inner class CoroutineLifecycleListener(private val job: Job) : LifecycleObserver {
+//
+//        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+//        fun cancelCoroutine() {
+//            LogUtils.d("mLifecycleOwner: ${job.isCancelled}")
+//            if (!job.isCancelled) {
+//                job.cancel()
+//            }
+//        }
+//    }
 }
